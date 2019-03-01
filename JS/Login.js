256f3549038094c64ae1b387efd6775c44e9be92
Jainam12 <43502575+Jainam12@users.noreply.github.com>
2019-03-01 19:24:18 +0530

    Create Login.js

---
+++ JS/Login.js
@@ -0,0 +1,48 @@
+function loginuser(){
+
+ var inputs = document.getElementsByTagName('input');
+for (var i = 0; i < inputs.length; i++) {
+    if(!inputs[i].checkValidity())
+      {
+        var id=inputs[i].id;
+        alert(id+" :"+inputs[i].validationMessage);
+        return;
+
+        //alert(inputs[i].validationMessage);
+      }
+}
+  var userEmail = document.getElementById("username").value;
+  var userPass = document.getElementById("password").value;
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then(function(user){
+       var user = firebase.auth().currentUser;
+       window.location.href= 'LabMain.php';
+    })
+    .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    
+
+
+    if(user != null){
+      var email_id = user.email;
+      console.log("Welcome User : " + email_id);
+      console.log(user.uid);
+    }
+  } else {
+    // No user is signed in.
+    console.log(user);
+  }
+});
+
