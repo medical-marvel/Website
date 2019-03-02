c7e521c65e37148aa842a12e860fb1715e56cef0
Riyajakhariya <47771335+Riyajakhariya@users.noreply.github.com>
2019-03-02 23:56:54 +0530

    Update AdminPage.php

--- AdminPage.php
+++ AdminPage.php
@@ -12,15 +12,15 @@ require_once 'Header.php';
 <body onload="load()">
 	<h1 class="w3-header">Regulatory Authority</h1>
 	<div class="w3-container w3-teal" style="margin: 16px;padding: 12px">
-		<h1>Hospitals</h1>
-		<div id="trialbar" class="w3-card-4"></div>
+		<h1>Doctors</h1>
+		<div id="trialbar"></div>
 		<div  id="trialinfo"> 
 			
 		</div>
 		<div class="w3-section" id="try" style="display:none;">
 				<button class="w3-button w3-green">Accept</button>
 				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
-			</div>
+			</div> 
 
 		<div id="DeclineReason" class="w3-modal">
 			<div class="w3-modal-content w3-card-4 w3-white">
@@ -42,8 +42,8 @@ require_once 'Header.php';
 		</div>
 	</div>
 	<div class="w3-container w3-teal" style="margin: 16px;padding: 12px">
-		<h1>Hospitals</h1>
-		<div id="trialbar1" class="w3-card-4"></div>
+		<h1>Labs</h1>
+		<div id="trialbar1"></div>
 		<div  id="trialinfo1"> 
 			
 		</div>
@@ -52,27 +52,27 @@ require_once 'Header.php';
 				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
 			</div>
 	</div>
-	<!-- <div class="w3-container w3-teal" style="margin: 16px;padding: 12px">
-		<h1>Doctor</h1>
-		<div class="w3-card w3-white" style="width: 300px;padding: 8px;margin: 8px;float: left;">
-			<h3>Name: </h3>
-			<label>Unique Identification Number: </label>
-			<div class="w3-section w3-center">
-				<button class="w3-button w3-green">Accept</button>
-				<button class="w3-button w3-red">Decline</button>
-			</div>
-		</div>
-	</div>
 	<div class="w3-container w3-teal" style="margin: 16px;padding: 12px">
 		<h1>Patient</h1>
-		<div class="w3-card w3-white" style="width: 300px;padding: 8px;margin: 8px;float: left;">
-			<h3>Name: </h3>
-			<label>Unique Identification Number: </label>
-			<div class="w3-section w3-center">
-				<button class="w3-button w3-green">Accept</button>
-				<button class="w3-button w3-red">Decline</button>
-			</div>
+		<div id="trialbar2"></div>
+		<div  id="trialinfo2"> 
+			
 		</div>
-	</div> -->
---></body>
+		<div class="w3-section" id="try2" style="display:none;">
+				<button class="w3-button w3-green">Accept</button>
+				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
+			</div>
+	</div>
+	<div class="w3-container w3-teal" style="margin: 16px;padding: 12px">
+		<h1>Trials</h1>
+		<div id="trialbar3"></div>
+		<div  id="trialinfo3"> 
+			
+		</div>
+		<div class="w3-section" id="try3" style="display:none;">
+				<button class="w3-button w3-green">Accept</button>
+				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
+			</div>
+	</div>
+</body>
 </html>
