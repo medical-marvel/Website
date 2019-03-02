5750e4a2081c8c9a2248856329f42b084b72a11a
Jainam12 <43502575+Jainam12@users.noreply.github.com>
2019-03-03 00:25:49 +0530

    Update Plot.php

--- Plot.php
+++ Plot.php
@@ -1,39 +1,53 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<script type="text/javascript">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+	<script type="text/javascript">
 
-window.onload = function () {
-	var chart = new CanvasJS.Chart("chartContainer", {
-		title:{
-			text: "My First Chart in CanvasJS"              
-		},
-		animationEnabled:true,
-		data: [              
-		{
-			// Change type to "doughnut", "line", "splineArea", etc.
-			type: "line",
-			dataPoints: [
-				{ label: "Insomnia",  y: 10  },
-				{ label: "Nausea", y: 15  },
-				{ label: "Constipation", y: 25  },
-				{ label: "Diarrhea",  y: 30  },
-				{ label: "Anxiety",  y: 28  },
-				{ label: "Head ache",  y: 45  },
-				{ label: "Stomach ache",  y: 30  },
-				{ label: "XYZ",  y: 19  },
-				{ label: "ABC",  y: 20  }
-			]
+		window.onload = function () {
+			var chart = new CanvasJS.Chart("chartContainer", {
+				title:{
+					text: "My First Chart in CanvasJS"
+				},
+				animationEnabled:true,
+				data: [
+				{
+					type: "line",
+					dataPoints:
+					[
+					{label: "Insomnia", y: 10},
+					{label: "Nausea", y: 15},
+					{label: "Constipation", y: 25},
+					{label: "Diarrhea", y: 30},
+					{label: "Anxiety", y: 28},
+					{label: "Head ache", y: 45},
+					{label: "Stomach ache", y: 30},
+					{label: "XYZ", y: 19},
+					{label: "ABC", y: 20}
+					]
+				},
+				{
+					type: "line",
+					dataPoints: [
+					{label: "Insomnia", y: 20},
+					{label: "Nausea", y: 5},
+					{label: "Constipation",y: 12},
+					{label: "Diarrhea", y: 10},
+					{label: "Anxiety", y: 35},
+					{label: "Head ache", y: 25},
+					{label: "Stomach ache", y: 30},
+					{label: "XYZ", y: 17},
+					{label: "ABC", y: 24}
+					]
+				}
+				]
+			});
+			chart.render();
 		}
-		]
-	});
-	chart.render();
-}
-</script>
+	</script>
 </head>
 <body>
-<div id="chartContainer" style="height: 300px; width: 100%;margin-top:100px;"></div>
+	<div id="chartContainer" style="height: 300px; width: 100%;margin-top:100px;"></div>
 </body>
 </html>
