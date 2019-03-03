0af5493c9818119fe1b826811def8df7c2905929
Riyajakhariya <47771335+Riyajakhariya@users.noreply.github.com>
2019-03-03 07:47:36 +0530

    Update TrialRequests.js

--- JS/TrialRequests.js
+++ JS/TrialRequests.js
@@ -1,4 +1,14 @@
 function myfunction()
 {
-	var patient=document.getElementBy
+	var user=firebase.auth().currentUser;
+	var database = firebase.firestore();
+	var i=0;
+	var cityRef = database.collection("Lab").doc(user.uid).collection("Trial").doc("").collection("Phase").doc().collection("Patient");
+	cityRef.get().then(function(result){
+		var status=result.Approval
+		database.doc("Lab/"+user.uid+"/Trial/Trial_"+i+"/Phase/Phase_"++"/Patient/"+user.uid).update(Approval).value
+		("Approved");
+		console.log(phasedata);
+	});
+
 }
