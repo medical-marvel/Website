bbda486e8e58ee0d98d355d4997d1fc25ae1b114
Jainam12 <43502575+Jainam12@users.noreply.github.com>
2019-03-03 12:12:48 +0530

    1

--- AddTrial.php
+++ AddTrial.php
@@ -14,35 +14,47 @@
 <body>
 	<h1><center><u>New Trials Details</u></center></h1>
 	<div class="try">
-		<div id="Trial_details">
+		<div id="trial_details">
 			<table>
-				<tr><th>Medicine Name</th><td><input required class="w3-input" type="textbox" id="Medicine_name" name="m1"></td></tr>
-				 <tr><th>Status</th><td><input required class="w3-input" type="textbox" id="Status" name="p1"></td></tr> 
-				<tr><th>Purpose</th><td><input required class="w3-input" type="textbox" id="Purpose" name="p1"></td></tr>
-				<!-- <tr><th>Specific Treatments</th><td><textarea id="Specific_treatment" maxlength="1000" rows="2" cols="10"></textarea></td></tr> -->
-				<tr><th>Age Group<br><label style="color: rgba(0,0,0,0.4);font-size: 12px">(0:Child,1:Adult,2:Old)</label></th><td><input type="text" name="age" id="Age"class="w3-input"></td></tr>
-				<tr><th>Allergies </th><td><input required class="w3-input" id="Allergies"></td></tr>
-				<!-- <tr><th>Recommended </th><td> <textarea required rows="2" id="Recommended" cols="10"></textarea></td></tr> -->
-				<tr><th>Not Recommended</th><td><input required class="w3-input" id="Not_recomended"></td></tr>
-				<tr><th>Number of Patients</th><td><input required class="w3-input" type="textbox" id="Patient_no" name="p1"></td></tr>
-<!-- 				<tr><th>WARNINGS AND PRECAUTIONS</th><td><textarea required rows="2" id="Warnings"  cols="10"></textarea></td></tr>
-				<tr><th>Do you have any Insurance ?</th><td><input required  type="radio" id="Insurance" name="n1" value="yes">Yes&nbsp&nbsp&nbsp&nbsp&nbsp<input type="radio" id="insurance"  name="n1" value="no">No</td></tr>
-				<tr><th>Insurance Number </th><td> <input reqiured class="w3-input" type="text" id="Insurance_no" name="i1"></td></tr> -->
+				<tr><th>Medicine Name</th><td><input required class="w3-input" type="textbox" id="medicine_name"></td></tr>
+				<tr><th>Status</th><td><input required class="w3-input" type="textbox" id="status"></td></tr> 
+				<tr><th>Purpose</th><td><input required class="w3-input" type="textbox" id="purpose"></td></tr>
+				<tr><th>Age Group<br><label style="color: rgba(0,0,0,0.4);font-size: 12px">(0:Child,1:Adult,2:Old)</label></th><td><input type="text" id="age"class="w3-input"></td></tr>
+				<tr><th>Allergies </th><td><input required class="w3-input" id="allergies"></td></tr>
+				<tr><th>Not Recommended</th><td><input required class="w3-input" id="not_recomended"></td></tr>
+				<tr><th>Number of Patients</th><td><input required class="w3-input" type="textbox" id="patient_no"></td></tr>
+				<tr><th>Number of Trials</th><td><input required class="w3-input" type="textbox" id="trial_no"></td></tr>
+				<tr><th>Success Rate</th><td><input required class="w3-input" type="textbox" id="success_rate"></td></tr>
 			</table>
 		</div>
-		 <div id="Phase_details" style="margin-top: 10px">
+		<div id="side_effects">
+			<h1><center><u>Possible Side Effects</u></center></h1>
+			<h4><center>(1:Yes & 0:No)</center></h4>
+			<table>
+				<tr><th>Headache</th><td><input required class="w3-input" type="input" id="headache" name="e1"></td></tr>
+				<tr><th>Stomachache</th><td><input required class="w3-input" type="input" id="stomachache" name="e2"></td></tr>
+				<tr><th>Nausea</th><td><input required class="w3-input" type="input" id="nausea" name="e3"></td></tr>
+				<tr><th>Constipation</th><td><input required class="w3-input" type="input" id="constipation" name="e4"></td></tr>
+				<tr><th>Insomnia</th><td><input required class="w3-input" type="input" id="insomnia" name="e5"></td></tr>
+				<tr><th>Fever</th><td><input required class="w3-input" type="input" id="fever" name="e6"></td></tr>
+				<tr><th>Diarrohea</th><td><input required class="w3-input" type="input" id="diarrohea" name="e7"></td></tr>
+				<tr><th>Food Appetite</th><td><input required class="w3-input" type="input" id="food_appetite" name="e8"></td></tr>
+				<tr><th>Anxiety</th><td><input required class="w3-input" type="input" id="anxiety" name="e9"></td></tr>
+			</table>
+		</div>
+		<div id="Phase_details" style="margin-top: 10px">
 			<h1><center><u>Session 1 Details</u></center></h1>
 			<table>
-				<tr><th>Starting Trial Date:</th><td><input required class="w3-input" type="Date" id="Starting_Date" name="s1"></td></tr>
-				<tr><th>Ending Trial Date:</th><td><input required class="w3-input" type="Date" id="Ending_Date" name="e1"></td></tr>
-				<tr><th>No. of Patients :</th><td><input required class="w3-input" type="textbor" id="No. of Patients" name="p1"></td></tr>
-				<tr><th>status :</th><td><input required class="w3-input" type="textbor" id="Status" name="p1"></td></tr>
-
-		</table>
+				<tr><th>Starting Trial Date:</th><td><input required class="w3-input" type="Date" id="starting_date" name="s1"></td></tr>
+				<tr><th>Ending Trial Date:</th><td><input required class="w3-input" type="Date" id="ending_date"></td></tr>
+				<tr><th>Status :</th><td><input required class="w3-input" type="textbor" id="status"></td></tr>
+			</table>
 		</div> 
+
+
 		<div class="w3-center">
-		<button style="margin-top: 10px;margin-bottom: 10px; " class=" w3-button w3-col s4 w3-bar-item w3-button w3-blue w3-round-xxlarge w3-display-center w3-hover-shadow w3-hover-blue w3-text-black" id="Addtrial" onclick="Addtrial()" style="width:100px;">Submit</button>
-	</div>
+			<button style="margin-top: 10px;margin-bottom: 10px; " class=" w3-button w3-col s4 w3-bar-item w3-button w3-blue w3-round-xxlarge w3-display-center w3-hover-shadow w3-hover-blue w3-text-black" id="addtrial" onclick="Addtrial()" style="width:100px;">Submit</button>
+		</div>
 	</div>
 </body>
 </html>
--- AdminPage.php
+++ AdminPage.php
@@ -4,91 +4,89 @@ require_once 'Header.php';
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- <link rel="stylesheet" href="PreviousTrials.css"> -->
 	<title>Administrator Page</title>
-	<link rel="stylesheet" href="CSS/AdminPage.css"> 
-<script src="JS/AdminPage.js"></script>
+	<script src="JS/AdminPage.js"></script>
 </head>
 <body onload="load()">
 	<h1 class="w3-header"><center>Regulatory Authority</center></h1>
 	<div class="w3-container" style="margin: 16px;padding: 12px;background-color: #ffffcc">
 		<h1>Doctors</h1>
 		<div id="trialnav">
-		<select class="Yes" id="trialbar" onchange="displayinfo()">
-  		<option value="volvo" disabled>Select Trial
-		</select>
+			<select class="Yes" id="trialbar" onchange="displayinfo()">
+				<option value="volvo" disabled>Select Trial</option>
+			</select>
 		</div>
-		<div  class="No" id="trialinfo"> 
-			
+		<div  id="trialinfo"> 
+
 		</div>
 		<div class="w3-section" id="try" style="display:none;">
-				<button class="w3-button w3-green">Accept</button>
-				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
-			</div> 
-
-		<div id="DeclineReason" class="w3-modal">
-			<div class="w3-modal-content w3-card-4 w3-white">
-				<header class="w3-container w3-grey"> 
-					<span onclick="document.getElementById('DeclineReason').style.display='none'" class="w3-button w3-display-topright">&times;</span>
-					<h4>Mention Reason of Declining</h4>
-				</header>
-				<div class="w3-dropdown-hover" style="width: 100%">
-					<button class="w3-button">Reason</button>
-					<div class="w3-dropdown-content w3-bar-block w3-border">
-						<a class="w3-bar-item w3-button">Not Registered on IRDAI</a>
-						<a class="w3-bar-item w3-button">Incomplete Information</a>
-						<a class="w3-bar-item w3-button">Information Mismatch</a>
-						<a class="w3-bar-item w3-button">Other</a>
-					</div>
+			<button class="w3-button w3-green">Accept</button>
+			<button class="w3-button w3-red" onclick="document.getElementById('decline_reason').style.display='block'">Decline</button> 
+		</div> 
+	</div>
+	<div id="decline_reason" class="w3-modal">
+		<div class="w3-modal-content w3-card-4 w3-white">
+			<header class="w3-container w3-grey"> 
+				<span onclick="document.getElementById('decline_reason').style.display='none'" class="w3-button w3-display-topright">&times;</span>
+				<h4>Mention Reason of Declining</h4>
+			</header>
+			<div class="w3-dropdown-hover" style="width: 100%">
+				<button class="w3-button">Reason</button>
+				<div class="w3-dropdown-content w3-bar-block w3-border">
+					<a class="w3-bar-item w3-button">Not Registered on IRDAI</a>
+					<a class="w3-bar-item w3-button">Incomplete Information</a>
+					<a class="w3-bar-item w3-button">Information Mismatch</a>
+					<a class="w3-bar-item w3-button">Other</a>
 				</div>
-				<button class="w3-button w3-center">Submit</button>
 			</div>
+			<button class="w3-button w3-center">Submit</button>
 		</div>
 	</div>
+
 	<div class="w3-container w3-teal" style="margin: 16px;padding: 12px">
 		<h1>Labs</h1>
 		<div id="trialnav">
-		<select class="Yes" id="trialbar1" onchange="displayinfo1()">
-  		<option value="volvo" disabled>Select Trial
-		</select>
+			<select class="Yes" id="trialbar1" onchange="displayinfo1()">
+				<option value="volvo" disabled>Select Trial</option>
+			</select>
 		</div>
-		<div  class="No" id="trialinfo1"> 
-			
+		<div  id="trialinfo1"> 
+
 		</div>
 		<div class="w3-section" id="try1" style="display:none;">
-				<button class="w3-button w3-green">Accept</button>
-				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
-			</div>
+			<button class="w3-button w3-green">Accept</button>
+			<button class="w3-button w3-red" onclick="document.getElementById('decline_reason').style.display='block'">Decline</button> 
+		</div>
 	</div>
 	<div class="w3-container" style="margin: 16px;padding: 12px;background-color: #ffffcc">
 		<h1>Patient</h1>
 		<div id="trialnav">
-		<select class="Yes" id="trialbar2" onchange="displayinfo2()">
-  		<option value="volvo" disabled>Select Trial
-		</select>
+			<select class="Yes" id="trialbar2" onchange="displayinfo2()">
+				<option value="volvo" disabled>Select Trial</option>
+			</select>
 		</div>
-		<div class="No" id="trialinfo2"> 
-			
+		<div id="trialinfo2"> 
+
 		</div>
 		<div class="w3-section" id="try2" style="display:none;">
-				<button class="w3-button w3-green">Accept</button>
-				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
-			</div>
+			<button class="w3-button w3-green">Accept</button>
+			<button class="w3-button w3-red" onclick="document.getElementById('decline_reason').style.display='block'">Decline</button> 
+		</div>
 	</div>
 	<div class="w3-container w3-teal" style="margin: 16px;padding: 12px">
 		<h1>Trials</h1>
 		<div id="trialnav">
-		<select class="Yes" id="trialbar3" onchange="displayinfo3()">
-  		<option value="volvo" disabled>Select Trial
-		</select>
+			<select class="Yes" id="trialbar3" onchange="displayinfo3()">
+				<option value="volvo" disabled>Select Trial</option>
+			</select>
 		</div>
-		<div class="No" id="trialinfo3"> 
-			
+		<div id="trialinfo3"> 
+
 		</div>
 		<div class="w3-section" id="try3" style="display:none;">
-				<button class="w3-button w3-green">Accept</button>
-				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
-			</div>
+			<button class="w3-button w3-green">Accept</button>
+			<button class="w3-button w3-red" onclick="document.getElementById('decline_reason').style.display='block'">Decline</button> 
+		</div>
 	</div>
 </body>
 </html>
