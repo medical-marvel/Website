e6710c91d7989c17de92397897c983d1e0cff938
RD99 <47771507+RD99@users.noreply.github.com>
2019-03-03 12:58:30 +0530

    Update ViewFeedback.js

--- JS/ViewFeedback.js
+++ JS/ViewFeedback.js
@@ -5,4 +5,12 @@ function Details()
 	var session=l.substring(l.indexOf("+p=")+3);
 	console.log(trial);
 	console.log(session);
+	var user=firebase.auth().currentUser;/*yash/Feedback/Feedback_1*/
+	var database = firebase.firestore();
+	database.collection("Lab/"+user.uid+"/Trial/"trial"/Phase/"session"/Patient")
+	.then(patients =>{
+		patients.forEach(patient =>{
+			console.log(patient);
+		})
+	})
 }
