39a0af4e94502d90ce4ffc91c4bb462c67b6deea
Riyajakhariya <47771335+Riyajakhariya@users.noreply.github.com>
2019-03-03 08:17:00 +0530

    Update TrialRequests.js

--- JS/TrialRequests.js
+++ JS/TrialRequests.js
@@ -5,10 +5,12 @@ function myfunction()
 	var i=0;
 	var cityRef = database.collection("Lab").doc(user.uid).collection("Trial").doc("").collection("Phase").doc().collection("Patient");
 	cityRef.get().then(function(result){
-		var status=result.Approval
-		database.doc("Lab/"+user.uid+"/Trial/Trial_"+i+"/Phase/Phase_"++"/Patient/"+user.uid).update(Approval).value
-		("Approved");
-		console.log(phasedata);
+		var status=result.Approval();
+		/*database.doc("Lab/"+user.uid+"/Trial/Trial_"+i+"/Phase/Phase_"++"/Patient/"+user.uid).update(Approval).value
+		("Approved");*/
+		db.collection("Lab").document().collection("Trial").document().collection("Phase").document().collection("Patient").update(
+   			{ "Apprroval": Approved}
+			);
 	});
 
 }
