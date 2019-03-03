7cd36d55997c3c794bd1200f0a18aa0c851ee1f5
Jainam12 <43502575+Jainam12@users.noreply.github.com>
2019-03-03 12:33:35 +0530

    Del Waste

--- TrialRequests.php
+++ TrialRequests.php
@@ -11,14 +11,14 @@ require_once 'Header.php';
 <body onload="load()">
 	<div id="trialnav" class="w3-card-4">
 		<select id="trialbar" onchange="displayinfo()">
-  <option value="volvo" disabled>Select Trial
-</select>
-</div>
-<div class=" w3-card-4" id="trialinfo" >
-</div>
-<div class="w3-section" id="try" style="display:none;">
-				<button class="w3-button w3-green" onclick="myfunction()">Accept</button>
-				<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
-			</div> 
+			<option value="volvo" disabled>Select Trial</option>
+		</select>
+	</div>
+	<div class=" w3-card-4" id="trialinfo" >
+	</div>
+	<div class="w3-section" id="try" style="display:none;">
+		<button class="w3-button w3-green" onclick="myfunction()">Accept</button>
+		<button class="w3-button w3-red" onclick="document.getElementById('DeclineReason').style.display='block'">Decline</button> 
+	</div> 
 </body>
 </html>
